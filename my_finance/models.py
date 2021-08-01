d85ce4fa951ca033d675deb20b9c03e3832f5dce
SergeiVorobev <vorobevse86@gmail.com>
2021-08-01 15:14:58 +0200

    second commit

--- my_finance/models.py
+++ my_finance/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+from django.utils import timezone
+
+class MyApp(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    desc = models.TextField(default='', blank=True)
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_apps')
+
+    def __str__(self):
+        return f'{self.id} {self.name}'
+
+    class Meta:
+        verbose_name_plural = 'my apps'
+        ordering = ['name']
+
+# class Balance(models.Model):
+#     BTC = models.DecimalField(max_digits=8, decimal_places=6)
+#     USDT = models.DecimalField(max_digits=9, decimal_places=2)
+#     date = models.DateField('Event Date', default=timezone.now)
+#
+#     def __str__(self):
+#         return self.name
--- my_finance/views.py
+++ my_finance/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from my_finance.models import MyApp
+
 
 # Create your views here.
+def home(request):
+    # name = "John"
+    all_apps = MyApp.objects.all()
+    context = {
+        'my_apps': all_apps
+    }
+    return render(request, 'website/index.html')
