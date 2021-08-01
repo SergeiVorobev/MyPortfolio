ebb22258323986ec48db7a29ee6c479e559314e2
SergeiVorobev <vorobevse86@gmail.com>
2021-08-01 17:42:06 +0200

    4 commit

--- MyPortfolio/asgi.py
+++ MyPortfolio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MyBalance project.
+ASGI config for MyPortfolio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBalance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyPortfolio.settings')
 
 application = get_asgi_application()
--- MyPortfolio/wsgi.py
+++ MyPortfolio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyBalance project.
+WSGI config for MyPortfolio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBalance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyPortfolio.settings')
 
 application = get_wsgi_application()
